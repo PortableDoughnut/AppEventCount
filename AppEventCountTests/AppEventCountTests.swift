38a662d3af348415ab8b4401d4f5abedc9343198
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-11 08:59:58 -0600

    Initial Commit

---
+++ AppEventCountTests/AppEventCountTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppEventCountTests.swift
+//  AppEventCountTests
+//
+//  Created by Gwen Thelin on 10/11/24.
+//
+
+import Testing
+@testable import AppEventCount
+
+struct AppEventCountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
