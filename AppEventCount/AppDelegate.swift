26a1df5ac35ed3fcb9c1525872258a1b2a9bbaea
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-11 09:23:26 -0600

    Initial Commit

--- AppEventCount/AppDelegate.swift
+++ AppEventCount/AppDelegate.swift
@@ -10,7 +10,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+	var launchCount: Int = 0
+	var configurationForConnectingCount: Int = 0
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
